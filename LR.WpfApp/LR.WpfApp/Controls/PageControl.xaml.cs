a43dc9db6426ff239e2a5bbffb756fd25a8da530
“e <ykai_167@163.com>
2020-05-09 22:32:52 +0800

    分页以及数据导出

---
+++ LR.WpfApp/LR.WpfApp/Controls/PageControl.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LR.WpfApp.Controls
+{
+    /// <summary>
+    /// PageControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class PageControl : UserControl
+    {        
+        public static RoutedEvent FirstPageEvent;
+        public static RoutedEvent PreviousPageEvent;
+        public static RoutedEvent NextPageEvent;
+        public static RoutedEvent LastPageEvent;
+
+        public static readonly DependencyProperty CurrentPageProperty;
+        public static readonly DependencyProperty TotalPageProperty;
+
+        public string CurrentPage
+        {
+            get { return (string)GetValue(CurrentPageProperty); }
+            set { SetValue(CurrentPageProperty, value); }
+        }
+
+        public string TotalPage
+        {
+            get { return (string)GetValue(TotalPageProperty); }
+            set { SetValue(TotalPageProperty, value); }
+        }
+
+        public PageControl()
+        {
+            InitializeComponent();
+        }
+
+        static PageControl()
+        {
+            FirstPageEvent = EventManager.RegisterRoutedEvent("FirstPage", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(PageControl));
+            PreviousPageEvent = EventManager.RegisterRoutedEvent("PreviousPage", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(PageControl));
+            NextPageEvent = EventManager.RegisterRoutedEvent("NextPage", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(PageControl));
+            LastPageEvent = EventManager.RegisterRoutedEvent("LastPage", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(PageControl));
+
+            CurrentPageProperty = DependencyProperty.Register("CurrentPage", typeof(string), typeof(PageControl), new PropertyMetadata(string.Empty, new PropertyChangedCallback(OnCurrentPageChanged)));
+            TotalPageProperty = DependencyProperty.Register("TotalPage", typeof(string), typeof(PageControl), new PropertyMetadata(string.Empty, new PropertyChangedCallback(OnTotalPageChanged)));
+        }
+
+        public event RoutedEventHandler FirstPage
+        {
+            add { AddHandler(FirstPageEvent, value); }
+            remove { RemoveHandler(FirstPageEvent, value); }
+        }
+
+        public event RoutedEventHandler PreviousPage
+        {
+            add { AddHandler(PreviousPageEvent, value); }
+            remove { RemoveHandler(PreviousPageEvent, value); }
+        }
+
+        public event RoutedEventHandler NextPage
+        {
+            add { AddHandler(NextPageEvent, value); }
+            remove { RemoveHandler(NextPageEvent, value); }
+        }
+
+        public event RoutedEventHandler LastPage
+        {
+            add { AddHandler(LastPageEvent, value); }
+            remove { RemoveHandler(LastPageEvent, value); }
+        }
+
+        public static void OnTotalPageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PageControl p = d as PageControl;
+
+            if (p != null)
+            {
+                Run rTotal = (Run)p.FindName("rTotal");
+
+                rTotal.Text = (string)e.NewValue;
+            }
+        }
+
+        private static void OnCurrentPageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PageControl p = d as PageControl;
+
+            if (p != null)
+            {
+                Run rCurrrent = (Run)p.FindName("rCurrent");
+
+                rCurrrent.Text = (string)e.NewValue;
+            }
+        }
+
+        private void FirstPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(FirstPageEvent, this));
+        }
+
+        private void PreviousPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(PreviousPageEvent, this));
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(NextPageEvent, this));
+        }
+
+        private void LastPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(LastPageEvent, this));
+        }
+    }
+}
