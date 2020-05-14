ad88c1336215a809b007832b032c7a9d396e6e93
jzf200617 <jzf200617@163.com>
2020-05-15 07:54:53 +0800

    不可重复启动

--- LR.WpfApp/LR.WpfApp/App.xaml.cs
+++ LR.WpfApp/LR.WpfApp/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace LR.WpfApp
 {
@@ -20,7 +23,28 @@ namespace LR.WpfApp
 
         private void App_Startup(object sender, StartupEventArgs e)
         {
+            var name = AppDomain.CurrentDomain.FriendlyName;
 
+            var old = System.Diagnostics.Process.GetProcessesByName(name.Replace(".exe", ""));
+            if (old.Length > 1)
+            {
+                var r = MessageBox.Show("程序启动, 是否关闭后重新启动?", "提示", MessageBoxButton.YesNo);
+                if (r == MessageBoxResult.Yes)
+                {
+                    old.OrderBy(o => o.StartTime).First().Kill();
+                }
+                else
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+
+            //UI线程未捕获异常处理事件
+            this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            //Task线程内未捕获异常处理事件
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            //非UI线程未捕获异常处理事件
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
             var baseType = typeof(LR.Services.IQueryService<>);
             var types = baseType.Assembly.GetTypes()
@@ -55,6 +79,47 @@ namespace LR.WpfApp
             {
                 this.Shutdown();
             }
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true; //把 Handled 属性设为true，表示此异常已处理，程序可以继续运行，不会强制退出      
+                MessageBox.Show("UI线程异常:" + e.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                //此时程序出现严重异常，将强制结束退出
+                MessageBox.Show("UI线程发生致命错误！");
+            }
+
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            StringBuilder sbEx = new StringBuilder();
+            if (e.IsTerminating)
+            {
+                sbEx.Append("非UI线程发生致命错误");
+            }
+            sbEx.Append("非UI线程异常：");
+            if (e.ExceptionObject is Exception)
+            {
+                sbEx.Append(((Exception)e.ExceptionObject).Message);
+            }
+            else
+            {
+                sbEx.Append(e.ExceptionObject);
+            }
+            MessageBox.Show(sbEx.ToString());
+        }
+
+        void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            //task线程内未处理捕获
+            MessageBox.Show("Task线程异常：" + e.Exception.Message);
+            e.SetObserved();//设置该异常已察觉（这样处理后就不会引起程序崩溃）
         }
     }
 }
