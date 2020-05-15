141139c0905e6241d77e4bbf0bbf2b8c86a49fcb
jzf200617 <jzf200617@163.com>
2020-05-15 22:03:15 +0800

    启动控制

---
+++ LR.WpfApp/LR.Services/LogService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR.Services
+{
+    public interface ILogService : IQueryService<LR.Entity.Log>
+    {
+        List<object> GetAll();
+    }
+
+    class LogService : QueryServiceBase<LR.Entity.Log>, ILogService
+    {
+        public List<object> GetAll()
+        {
+            return this.Context.Context.Queryable<Entity.Log, Entity.Admin>((log, admin) => log.OperatorID == admin.ID)
+                 .Select((log, admin) => (object)new
+                 {
+                     Operator = admin.Name,
+                     Table = log.Table,
+                     Data = log.Data,
+                     CreateDate = log.CreateDate,
+                 }).ToList();
+        }
+    }
+}
---
+++ LR.WpfApp/LR.WpfApp/Controls/LogsControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LR.WpfApp.Controls
+{
+    /// <summary>
+    /// LogsControl.xaml 的交互逻辑
+    /// </summary>
+    [UserControlUse(UseTo.SuperAdminWindow, TabHeader = "日志列表", Order = 30)]
+    public partial class LogsControl : UserControl
+    {
+        LR.Services.ILogService _service;
+
+        public LogsControl(LR.Services.ILogService service)
+        {
+            InitializeComponent();
+            this._service = service;
+
+            this.InitListView();
+        }
+        
+        private void InitListView()
+        {
+            this.lvwShow.ItemsSource = this._service.GetAll();
+        }        
+    }
+}
